2cf858cdb6c6938e160e7937bfa48c40c93efaf3
Takn <virgile.prideaux@epitech.eu>
2015-06-03 16:40:24 +0200

    indentations

--- Fragments/ExampleFragment.java
+++ Fragments/ExampleFragment.java
@@ -1,27 +1,27 @@
 public class ExampleFragment extends Fragment {
 	
 	private Context	context;
-    private View	rootView;
+	private View	rootView;
 	
 	@Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-		
+	public void onActivityCreated(Bundle savedInstanceState) {
+	        super.onActivityCreated(savedInstanceState);
+			
 		/*
 		** Here you may start accessing views from rootView
 		** rootView.findViewById(...);
 		*/
-    }
+    	}
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        context = activity;
-    }
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		context = activity;
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_example, container, false);
-        return rootView;
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		rootView = inflater.inflate(R.layout.fragment_example, container, false);
+		return rootView;
+	}
 }
