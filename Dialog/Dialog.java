77a7ae1aff2bc05b390735a67a11d60f928ed45e
virgile <virgile.prideaux@epitech.eu>
2015-07-06 15:31:16 +0200

    Added Dialog

---
+++ Dialog/Dialog.java
@@ -0,0 +1,25 @@
+package com.myapplication;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class Dialog {
+    public static void alert(Context c, String title, String message, String button, DialogInterface.OnClickListener callBack) {
+        AlertDialog.Builder b = new AlertDialog.Builder(c);
+        b.setTitle(title);
+        b.setMessage(message);
+        b.setNeutralButton(button, callBack);
+        b.create().show();
+    }
+
+    public static android.app.Dialog progress(Context c, String title, String message) {
+        ProgressDialog d = new ProgressDialog(c);
+        d.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        d.setTitle(title);
+        d.setMessage(message);
+        d.show();
+        return d;
+    }
+}
