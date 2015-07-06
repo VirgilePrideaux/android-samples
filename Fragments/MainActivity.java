97e4621c9bd2b5e12c4ae6b771b34f9643397ef6
virgile <virgile.prideaux@epitech.eu>
2015-07-06 15:25:27 +0200

    non-content modifications

--- Fragments/MainActivity.java
+++ Fragments/MainActivity.java
@@ -1,6 +1,6 @@
 public class MainActivity extends Activity {
-	@Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override
+	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		
